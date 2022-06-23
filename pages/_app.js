28a88ba801fd74b1b3b8b82f684c590693051de6
Shaun Carr <shaunc138@gmail.com>
2022-06-23 14:56:25 -0400

    Move Head to _app, built desktop header section

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Shaun Carr</title>
+        <meta name="description" content="shauncarr.dev" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
-import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-2">
-      <Head>
-        <title>Shaun Carr</title>
-        <meta name="description" content="shauncarr.dev" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <main className="flex w-full flex-col justify-center items-center text-center">
-        <h1 className="font-bold lg:text-7xl text-6xl text-fuchsia-600">
-          Hello I&apos;m Shaun.
-        </h1>
-        <p className="lg:text-2xl text-xl mt-10 text-neutral-500">
-          Welcome to my web garden 🌱
-        </p>
+    <div className="min-h-screen p-2 bg-neutral-50 selection:bg-blue-600/90 selection:text-white">
+      <main className="relative w-full mx-auto sm:max-w-screen-sm px-4 pt-48">
+        <div>
+          <section>
+            <div className="flex items-center space-x-10">
+              <div className="rounded-full h-[200px] w-[200px] overflow-hidden">
+                <Image
+                  alt="picture of me"
+                  src="/images/profile-pic.jpg"
+                  width={597}
+                  height={597}
+                />
+              </div>
+              <div>
+                <h1 className="text-4xl font-bold">
+                  Hello I&apos;m Shaun,
+                  <br />a web developer.
+                </h1>
+                <h2 className="text-neutral-500 text-xl mt-2">
+                  Welcome to <i>my</i> corner of the internet 👋
+                </h2>
+                <div className="mt-6 text-xl font-bold text-blue-700">
+                  <a href="#">contact me</a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
