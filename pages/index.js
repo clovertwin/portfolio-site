3de77f082cc67afa5d62d29574df8576b39b4284
Shaun Carr <shaunc138@gmail.com>
2022-06-23 21:37:02 -0400

    Write header description, thicken hr, add ring to image

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-2 bg-neutral-50 selection:bg-blue-600/90 selection:text-white">
+    <div className="min-h-screen bg-neutral-50 selection:bg-blue-600/90 selection:text-white">
       <main className="font-inter relative w-full mx-auto px-4 pt-20 sm:max-w-screen-sm sm:pt-40">
         <div>
           <section>
-            <div className="flex flex-col items-center justify-center sm:space-x-10 sm:flex-row">
-              <div className="rounded-full h-[155px] w-[155px] overflow-hidden">
+            <div className="flex flex-col items-center justify-center sm:space-x-10 sm:flex-row sm:justify-start">
+              <div className="rounded-full h-[150px] w-[150px] overflow-hidden ring-4 ring-blue-600">
                 <Image
                   alt="picture of me"
                   src="/images/profile-pic.jpg"
@@ -16,21 +16,24 @@ export default function Home() {
                   priority
                 />
               </div>
-              <div className="mt-2 text-center sm:mt-0 sm:text-left">
-                <h1 className="font-manrope text-3xl font-bold">
+              <div className="mt-5 text-center sm:mt-3 sm:text-left">
+                <h1 className="font-manrope text-3xl font-extrabold sm:text-5xl">
                   Hello I&apos;m Shaun,
                   <br />a web developer.
                 </h1>
-                <h2 className="text-neutral-500 text-lg mt-2">
-                  Welcome to <i>my</i> corner of the internet 👋
-                </h2>
-                <div className="mt-6 text-lg font-bold text-blue-700">
-                  <a href="#">contact me</a>
-                </div>
               </div>
             </div>
+            <h2 className="text-neutral-500 text-lg mt-5 text-center sm:text-xl sm:text-left">
+              <strong>Welcome</strong> to my <i>corner</i> of the internet ✌️.{" "}
+              <strong>
+                This is where I share everything I&apos;m working on and
+                learning
+              </strong>
+              , as well as serving as a <i>digital garden</i> where I grow my
+              tech career 🌱.
+            </h2>
           </section>
-          <hr className="mt-16" />
+          <hr className="mt-5 border-2 border-neutral-200 rounded"></hr>
         </div>
       </main>
     </div>
