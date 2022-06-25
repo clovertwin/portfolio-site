70b80f1c8d4014153b238a7d7a78e2ca9e25006a
Shaun Carr <shaunc138@gmail.com>
2022-06-25 13:24:00 -0400

    Edit about me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -27,15 +27,14 @@ export default function AboutMe() {
           {/* The <i>crazy</i> thing is, this dosen&apos;t really feel like work to
           me.  */}
           As I journey further into <strong>CSS, Javascript, and React</strong>,
-          and the challenges grow <i>steeper</i>, I get <i>more excited</i> to
-          put my skills to use and see what I&apos;ve ive learned come to life
-          on the internet.
+          and the challenges grow steeper, I get more excited to put my skills
+          to use and see what I&apos;ve built come to life on the internet.
         </p>
         <br />
         <p>
           Now, having never gone to college for computer science or ever working
           at a tech company before, I dont have much to list here for
-          experience. <i>What I do have</i> is a{" "}
+          experience. What I do have is a{" "}
           <strong>
             burning desire to step up and take my learning to the next level,
             with a company looking for someone who loves a challenge
