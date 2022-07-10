c3a70c84c6bd0e437e88002267014e750b1bf166
Shaun Carr <shaunc138@gmail.com>
2022-07-10 16:11:44 -0400

    Adjust margins

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -30,10 +30,12 @@ export default function SinglePostPage({ post }) {
           <a>&#8592; Back to all posts</a>
         </Link>
       </div>
-      <div className="border-b-4 border-blue-600 inline-block mb-20">
-        <h1 className="font-manrope tracking-tighter text-slate-800 font-extrabold text-4xl sm:text-5xl">
-          {post.title}
-        </h1>
+      <div className="text-center">
+        <div className="border-b-4 border-blue-600 inline-block mb-10 mt-10 sm:mb-20 sm:mt-0">
+          <h1 className="font-manrope tracking-tighter text-slate-800 font-extrabold text-4xl sm:text-5xl">
+            {post.title}
+          </h1>
+        </div>
       </div>
       <div className="prose prose-neutral sm:prose-lg">
         <MDXContent />
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,10 +8,12 @@ export const getStaticProps = () => {
 export default function PostListPage({ posts }) {
   return (
     <div>
-      <div className="border-b-4 border-blue-600 inline-block mb-10">
-        <h1 className="font-manrope tracking-tighter text-slate-800 font-extrabold text-4xl sm:text-5xl">
-          Blog posts
-        </h1>
+      <div className="text-center">
+        <div className="border-b-4 border-blue-600 inline-block mb-5 sm:mb-10">
+          <h1 className="font-manrope tracking-tighter text-slate-800 font-extrabold text-4xl sm:text-5xl">
+            Blog posts
+          </h1>
+        </div>
       </div>
       <div className="prose prose-blue">
         <ul>
