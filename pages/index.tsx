9fd84fa78f2dc8ff44b04adc23e3289e460e03d8
Shaun Carr <shaunc138@gmail.com>
2022-09-16 11:59:58 -0400

    Update dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import { compareDesc } from "date-fns";
-import { allPosts, type Post } from "contentlayer/generated";
+import { allPosts } from "contentlayer/generated";
 import Header from "../components/Header";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
 
-export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const posts = allPosts
     .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
     .slice(0, 3);
