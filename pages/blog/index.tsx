4bcc9dc2bbfc834fd8769457de903c6633aabe56
Shaun Carr <shaunc138@gmail.com>
2022-09-27 23:22:13 -0400

    Adjust getStaticProps to not return post body

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,19 +1,31 @@
-import { compareDesc, format, parseISO } from "date-fns";
-import { allPosts, Post } from "contentlayer/generated";
+import { compareDesc, format } from "date-fns";
+import { allPosts } from "contentlayer/generated";
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
+interface BlogPost {
+  slug: string;
+  date: string;
+  description: string;
+  title: string;
+}
+
 export const getStaticProps: GetStaticProps = async () => {
-  const posts: Post[] = allPosts.sort((a, b) =>
-    compareDesc(new Date(a.date), new Date(b.date))
-  );
+  const posts = allPosts
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+    .map((post) => ({
+      slug: post.slug,
+      date: post.date,
+      description: post.description,
+      title: post.title,
+    }));
   return { props: { posts } };
 };
 
 export default function PostListPage({
   posts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const dates = posts.map((post: Post) => {
+  const dates = posts.map((post: BlogPost) => {
     let dt = new Date(post.date.split("T")[0]);
     return new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
   });
@@ -29,7 +41,7 @@ export default function PostListPage({
       </div>
       <div className="text-lg sm:text-xl ">
         <ul>
-          {posts.map((post: Post, id: number) => (
+          {posts.map((post: BlogPost, id: number) => (
             <li
               key={post.slug}
               className="mt-5 pl-4 flex flex-col justify-start sm:flex-row sm:pl-0"
@@ -45,7 +57,7 @@ export default function PostListPage({
               </p>
               <Link href={`/blog/${post.slug}`}>
                 <a
-                  aria-label="link to full blog post"
+                  aria-label={`link to blog post titled ${post.title}`}
                   className="text-blue-600 hover:text-blue-400 dark:text-sky-500 dark:hover:text-sky-600"
                 >
                   read more...
