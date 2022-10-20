0503284ee1bd681b2f901065bd7cf4b70e5ae6d1
Shaun Carr <shaunc138@gmail.com>
2022-10-20 11:21:34 -0400

    Reduce time of lcp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,16 +18,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            // crossOrigin="true"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Manrope:wght@700;800&display=swap"
             rel="stylesheet"
           />
+          <link rel="preload" as="image" href="/images/profile-pic.jpg" />
         </Head>
         <body>
           <Main />
