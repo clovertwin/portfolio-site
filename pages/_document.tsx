5d0d7187787d2dc93af004707a06cd1ee08b9519
Shaun Carr <shaunc138@gmail.com>
2023-03-14 23:53:49 -0400

    fix bug in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,11 +19,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Manrope:wght@700;800&display=swap"
             rel="stylesheet"
